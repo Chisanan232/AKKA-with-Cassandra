393fc2809b7946731da4ad3454dc93cfacbc0937
Chisanan232 <chi10211201@cycu.org.tw>
2020-07-13 14:28:58 +0800

    Note about Spark Cassandra connector saving mode

--- src/main/scala/SparkSaver.scala
+++ src/main/scala/SparkSaver.scala
@@ -23,7 +23,17 @@ class SparkSaver {
     // Show what data in it
     jsonData.show()
     // Save it to database
-    jsonData.toDF().write.format("org.apache.spark.sql.cassandra").options(Map("keyspace" -> "test_keyspace", "table" -> "testcafe")).save()
+    // Note: Writing data with different mode has different effect
+    // https://stackoverflow.com/questions/31844318/update-cassandra-table-using-spark-cassandra-connector
+    // https://www.itread01.com/content/1547798588.html
+    // Also could refer to the Python version info
+    // https://www.twblogs.net/a/5cc6c662bd9eee1ac2ed6083
+    // Source Code below:
+    // https://github.com/apache/spark/blob/v1.5.1/sql/core/src/main/scala/org/apache/spark/sql/DataFrameWriter.scala#L41
+    jsonData.toDF().write
+      .format("org.apache.spark.sql.cassandra")
+      .options(Map("keyspace" -> "test_keyspace", "table" -> "testcafe"))
+      .save()
   }
 
 
